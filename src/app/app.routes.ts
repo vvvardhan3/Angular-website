65ff8f5450ac307bf410712c42c8f8bf4532288f
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2025-02-09 05:58:29 +0530

    added google signin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,13 +8,12 @@ import { CertificationsComponent } from './certifications/certifications.compone
 import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
-
-    {path:'home', component: LandingComponent},
-    {path:'education', component: EducationComponent},
-    {path:'work-experience', component: WorkExperienceComponent},
-    {path:'projects', component: ProjectsComponent},
-    {path:'skills', component: SkillsComponent},
-    {path:'certifications', component: CertificationsComponent},
-    {path:'contactus', component: ContactComponent},
-    
+  { path: 'home', component: LandingComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'work-experience', component: WorkExperienceComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'certifications', component: CertificationsComponent },
+  { path: 'contactus', component: ContactComponent },
+  { path: '', component: LandingComponent },
 ];
